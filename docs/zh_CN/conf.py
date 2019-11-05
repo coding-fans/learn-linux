e33f3ef2e702907484b280fcccb18bbf935699a3
Fasion Chan <fasionchan@gmail.com>
2019-11-05 09:31:05 +0800

    add deploy script

--- docs/zh_CN/conf.py
+++ docs/zh_CN/conf.py
@@ -92,7 +92,7 @@ html_theme_options = {
     'logo': 'logo.jpg',
     #'logo_name': True,
     'description': u'跟着小菜一起学习Linux！',
-    'canonical_url': 'https://' + site_domain + '/zh_CN/latest/',
+    'canonical_url': ('https://' + site_domain + '/zh_CN/latest/'),
 
     'font_family': ','.join(map(repr, [
         'Arial',
